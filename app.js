6481290853273f8e4a40a8f3aff35cb0129c55ec
ShubhSharma <65842507+shubhsharma0333@users.noreply.github.com>
2021-04-24 20:58:50 +0530

    port changes

--- app.js
+++ app.js
@@ -161,10 +161,10 @@ app.get("/logout", function(req, res){
 });
 
 let port = process.env.PORT;
-if (port == null || port == "") {
+if(port ==null || port== ""){
   port = 3000;
 }
 
 app.listen(port, function() {
-  console.log("Server has started successfully.")
+  console.log("Server started on port 3000");
 });
